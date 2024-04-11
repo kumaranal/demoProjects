332ba0e78faea6f61577bf8d12cebdf221850e10
kumaranal <analbiswas180@gmail.com>
2024-04-11 12:48:14 +0530

    push

--- src/app/api/check/route.ts
+++ src/app/api/check/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: NextRequest,res: NextResponse) {
     
   console.log("data",databody,process.env.ENVIRONMENT)
 
-  return NextResponse.json({data:databody,env:process.env.ENVIRONMENT}  , { status: 200 })
+  return NextResponse.json({dataprod:databody,env:process.env.ENVIRONMENT}  , { status: 200 })
 
 };
 
